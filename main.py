80328de5a7db5fbeafd156aba66e1236353f4a14
TenzinGayche <gaychetenzin@gmail.com>
2025-11-19 23:04:39 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def gangster_name_generator(name):
+    return f"The {name} Gangster"
+
+print(gangster_name_generator("John"))
+print(gangster_name_generator("Jane"))
+print(gangster_name_generator("Jim"))
+print(gangster_name_generator("Jill"))
+print(gangster_name_generator("Jack"))
+print(gangster_name_generator("Jill"))
+print(gangster_name_generator("Jill"))
